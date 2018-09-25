caf39e2dc3fa2b0bdcaf9aabb3aeed83a10c4241
mymymy1303 <thehuy160897@gmail.com>
2018-09-25 23:57:16 +0700

    Configure ui-bootstrap4

--- tasks/concat-js.js
+++ tasks/concat-js.js
@@ -6,8 +6,8 @@ module.exports = function(gulp, $, browserSync) {
                 'bower_components/angular-ui-router/release/angular-ui-router.min.js',
                 'bower_components/angular-animate/angular-animate.min.js',
                 'bower_components/angular-sanitize/angular-sanitize.min.js',
-                'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-                'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                'node_modules/ui-bootstrap4/dist/ui-bootstrap.js',
+                'node_modules/ui-bootstrap4/dist/ui-bootstrap-tpls.js',
                 'bower_components/angular-toastr/dist/angular-toastr.min.js',
                 'bower_components/angular-toastr/dist/angular-toastr.tpls.min.js',
             ])
